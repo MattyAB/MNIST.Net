baddf52ee2d354d86b8ef555264f6ab642de2aa6
MattyAB <matthew.beton@gmail.com>
2017-10-07 13:45:26 +0100

    Added basic code and csvs.

--- MNISTNet/Program.cs
+++ MNISTNet/Program.cs
@@ -4,8 +4,13 @@ namespace MNISTNet
 {
     class Program
     {
+        const string test = @"D:\Documents\GitHub\MNISTNet\test.csv";
+        const string train = @"D:\Documents\GitHub\MNISTNet\train.csv";
+
         static void Main(string[] args)
         {
+            DataLoader dl = new DataLoader(test, train);
+
             Console.WriteLine("Hello World!");
         }
     }
