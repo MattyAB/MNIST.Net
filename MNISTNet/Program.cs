82ac428cc2779ebd908b3b0874364e7cbb8092b8
MattyAB <matthew.beton@gmail.com>
2017-10-07 13:36:19 +0100

    Add project files.

---
+++ MNISTNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MNISTNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
